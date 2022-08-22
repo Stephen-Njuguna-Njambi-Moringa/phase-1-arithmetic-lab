3f7e3755aa6fbcc86938bb941f0f0530ef6b8ad3
Stephen Njuguna Njambi <stepehen.jambi@student.moringaschool.com>
2022-08-22 23:26:51 +0300

    lab passed

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 
 // Write your code here
+let num1=2, num2=31, num3=16, num4=4;
+     
+
+const multiply=num1*num2;
+const random=Math.floor(Math.random() * 10) + 1;
+const mod=num3/num4;
+const max=Math.max(10, 20);;
